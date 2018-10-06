db2e3da0766f86a5884a0cf30644baa2e928b3c5
Carl Canotal <carl.canotal@telus.com>
2018-10-06 15:29:24 -0700

    Simple implementation of http server

--- api.js
+++ api.js
@@ -1,10 +1,34 @@
 var database = require('./database');
 var fs = require('fs');
 var http = require('http');
+var URL = require('url');
 
 var server = http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.end('API server is running');
+	res.writeHead(200, {'Content-type' : 'application/json'});
+	
+	// determine what the incoming URL request is
+	var parsedUrl = URL.parse(req.url, true);
+	console.log(parsedUrl);
+	switch(parsedUrl.pathname) {
+		case 'tienda/register':
+			if(req.method == 'POST') {
+				var body = "";
+				// get the passed data during POST during req 'on data' event
+				req.on('data', function(dataChunk) {
+					body += dataChunk;
+				});
+				
+				req.on('end', function() {
+					// Once data is completed from POST body turn it into JSON to proceed with saving to DB
+					var postJSON = JSON.parse(body);
+					console.log(postJSON);
+					
+				});
+			}
+		break;
+		default:
+		res.end('That service is not available');
+	}
 });
 
 server.listen(9090);
